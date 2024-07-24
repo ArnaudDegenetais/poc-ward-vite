3ad54b65231197d42977d85c68d6daa134710fe7
Arnaud Dégenétais <adegenetais@em-normandie.fr>
2024-07-24 09:42:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import Home from './pages/Home.vue'
+import About from './pages/About.vue'
+import NotFound from './pages/NotFound.vue'
+
+const routes: { [key: string]: any } = {
+    '/': Home,
+    '/about': About,
+}
+
+const currentPath = ref(window.location.hash)
+window.addEventListener('hashchange', () => {
+    currentPath.value = window.location.hash
+})
+const currentView = computed(() => {
+    return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
+</script>
+
+<template>
+    <div>
+        <a href="#/">Home</a> | <a href="#/about">About</a> |
+        <a href="#/non-existent-path">Broken Link</a>
+        <component :is="currentView" />
+    </div>
+</template>
+
+<style scoped>
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+}
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
